e8f7d88273f88463dd798b9d50f39fcb73339d33
Slava <splakhin@yahoo.com>
2025-04-14 10:52:45 -0500

    new chages

--- tests/userForm.spec.js
+++ tests/userForm.spec.js
@@ -13,7 +13,7 @@ test('Authenticate existing User', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Your Password' }).fill('test');
   await page.getByRole('textbox', { name: 'Your Password' }).press('Tab');
   await page.getByRole('button', { name: 'Authenticate' }).click();
-
+  await page.waitForLoadState();
   // not working cause, there is 3 input[type="text", and non of the <input> does not have "name"
   // expect (await page.locator('input[type="text"]').getAttribute('placeholder')).toBe("Description"); 
   // but playwright good in identifying <input> aka "textbox"
@@ -21,7 +21,7 @@ test('Authenticate existing User', async ({ page }) => {
   //following, because they have unique type - easy to capture by locator
   expect (await page.locator('input[type="number"]').getAttribute('placeholder')).toBe("Total Amount");
   expect (await page.locator('input[type="date"]')).toBeVisible();
-  await page.waitForLoadState();
+  
   expect (await page.getByRole('heading', { name: 'Add Expense for test' })).toBeVisible(); 
 });
 
