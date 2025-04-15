0f3d419a36c0c47f977bc9a2b1b5306d46af54f4
Slava <splakhin@yahoo.com>
2025-04-14 20:46:03 -0500

    new chages

--- tests/userForm.spec.js
+++ tests/userForm.spec.js
@@ -6,6 +6,28 @@ test.beforeEach(async ({ page }) => {
   await page.waitForLoadState();
 });
 
+
+
+
+//{"success":true,"expenses":[{"id":56,"userId":46,"transDescr":"nothing","transDate":"2025-03-01T00:00:00.000Z","transTotal":5},{"id":55,"userId":46,"transDescr":"nothing1","transDate":"2025-01-30T00:00:00.000Z","transTotal":80}]}
+test('Api test --- GET', async ({ request }) => {
+  const resp = await request.get("/getExpenses?userId=46"); 
+  
+  expect (resp.status()).toBe(200);
+  
+  const text = await resp.text();
+  console.log("text from GET: " + text);
+
+  const bodyRaw = await resp.body();
+  const body = bodyRaw.toString();
+  const respBody =  JSON.parse(body);
+  console.log("body from GET: " + body);
+  // Access specific keys-values in respBody
+  console.log("specific values from: respBody.expenses[0].userId=" + respBody.expenses[0].userId + 
+    ", respBody.expenses[0].transDescr="+respBody.expenses[0].transDescr);
+  expect (respBody.expenses[0].userId == 46 && respBody.expenses[0].transDescr == "nothing").toBeTruthy();
+});
+
 test('Authenticate existing User', async ({ page }) => {
   await page.getByRole('img', { name: 'DB' }).click();
   await page.getByRole('textbox', { name: 'Your User Name' }).fill('test');
@@ -16,8 +38,8 @@ test('Authenticate existing User', async ({ page }) => {
   await page.waitForLoadState('domcontentloaded');
   // not working cause, there is 3 input[type="text", and non of the <input> does not have "name"
   // but playwright can in identifying <input> aka "textbox", but better approach to get it by getByPlaceholder
-  expect (page.getByPlaceholder('Description')).toBeVisible();
-  expect ( page.getByPlaceholder('Total Amount')).toBeVisible();
+  expect (await page.getByPlaceholder('Description')).toBeVisible();
+  expect (await page.getByPlaceholder('Total Amount')).toBeVisible();
   //following, because they have unique type - easy to capture by locator
   // expect (await page.locator('input[type="date"]').isVisible()).toBeTruthy();
   
