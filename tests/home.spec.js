0b28cb0e4b05fc59d7c6ebb9f1e5e0555cd047d3
Slava <splakhin@yahoo.com>
2025-05-07 13:27:10 -0500

    new chages

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import utils from './utils.js';
 
+test.describe.configure({ retries: 5 });
+
 let intVal = utils.getRandomInt();
 // test.beforeEach(async ({ page }) => {
 //   await page.goto('/');
@@ -8,7 +10,7 @@ let intVal = utils.getRandomInt();
 //   await page.getByRole('img', { name: 'Contact' }).click();
 // });
 
-test('flacky test checking for o', async ( ) => {
+test('flacky test checking for even number (test.describe.configure({ retries: 5 });)', async ( ) => {
     console.log("test "+ intVal);
     expect (intVal%2 == 0).toBeTruthy();
   });
